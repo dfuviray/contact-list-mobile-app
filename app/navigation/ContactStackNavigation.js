cfdcd433620e4912185935ee4637df557213ac9e
dfuviray <deo.uviray@gmail.com>
2020-07-15 18:40:35 -0700

    Add header styles and statusbar

--- app/navigation/ContactStackNavigation.js
+++ app/navigation/ContactStackNavigation.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 
+import {colors} from '../config/colors';
 import Contact from '../screens/Contact/Contact';
 import Profile from '../screens/Profile/Profile';
 
 const ContactStack = createStackNavigator();
 
 export default function ContactStackNavigation() {
+  headerOptions = {
+    headerStyle: {
+      backgroundColor: colors.white,
+    },
+    headerTintColor: colors.primary,
+    headerTitleStyle: {alignSelf: 'center'},
+  };
+
   return (
-    <ContactStack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}>
-      <ContactStack.Screen name="Contact" component={Contact} />
-      <ContactStack.Screen name="Profile" component={Profile} />
-    </ContactStack.Navigator>
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor={colors.white} />
+      <ContactStack.Navigator screenOptions={headerOptions}>
+        <ContactStack.Screen name="Contact" component={Contact} />
+        <ContactStack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Profile"
+          component={Profile}
+        />
+      </ContactStack.Navigator>
+    </>
   );
 }
