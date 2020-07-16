0c5ac1ee6519dcd0622d63577f601414bf2d6848
dfuviray <deo.uviray@gmail.com>
2020-07-16 10:27:06 -0700

    Refactor useApi hook and add AsyncStorage

--- app/hooks/useApi.js
+++ app/hooks/useApi.js
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import axios from 'axios';
 import {useState, useEffect} from 'react';
 
@@ -17,18 +18,51 @@ const useApi = () => {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  setAsyncData = async (data) => {
+    try {
+      const jsonData = JSON.stringify(data);
+      await AsyncStorage.setItem('contact-list', jsonData);
+    } catch (error) {
+      console.log('SETTING ASYNC STORAGE #####', error);
+    }
+  };
+
+  getAsyncData = async () => {
+    try {
+      const jsonData = await AsyncStorage.getItem('contact-list');
+      return jsonData != null ? JSON.parse(jsonData) : [];
+    } catch (error) {
+      console.log('GETTING ASYNC STORAGE #####', error);
+    }
+  };
+
   const request = async () => {
     try {
       setError(false);
       setLoading(true);
 
       const response = await api.get(endpoint);
+      const {data: responseData} = response;
+
+      if (!responseData) {
+        setLoading(false);
+        const getData = await getAsyncData();
+        setData(getData);
+      }
 
       setLoading(false);
-      setData(response.data);
+      setAsyncData(responseData);
+      setData(responseData);
     } catch (error) {
-      setError(true);
       setLoading(false);
+      setError(false);
+
+      const getData = await getAsyncData();
+
+      if (getData) setData(getData);
+
+      if (!getData) setError(true);
+
       console.log('API #####', error);
     }
   };
